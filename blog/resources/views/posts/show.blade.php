7fd135b2a63439b1b6fa56945dea13e0ef5ebb84
Raul Nabarret <raulnabarret@Admins-MacBook-Pro.local>
2017-02-20 21:44:45 -0600

    Eloquent Relationships and Comments

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -7,7 +7,11 @@
 	<h1>{{ $post->title }}</h1>
 	<hr>
 	<p>{{ $post->body }}</p>
-
+	<hr>
+	<h4>Comments</h4>
+	@foreach($post->comments as $comment)
+		<p>{{ $comment->body }}</p>
+	@endforeach
 </div>
 
 	
